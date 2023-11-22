eea389e3503c1b72063a02b2a1ef52e2c03b57be
richache <richardchen2016@163.com>
2023-11-22 17:58:02 +0800

    Feature: 实现url parse

--- react-parser-url/src/App.js
+++ react-parser-url/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const defaultParserResult = {
   protocol: '',
@@ -11,8 +11,32 @@ const defaultParserResult = {
 }
 
 const parserUrl = (url) => {
-  // 你的实现
-  return defaultParserResult;
+  // 判断url格式正确性,内容是否为空
+  try {
+    if (url) {
+      const urlobj = new URL(url)
+      defaultParserResult.protocol = urlobj.protocol;
+      defaultParserResult.hostname = urlobj.hostname;
+      defaultParserResult.port = urlobj.port;
+      defaultParserResult.pathname = urlobj.pathname;
+      defaultParserResult.hash = urlobj.hash;
+      defaultParserResult.params = urlobj.search ? parseQueryString(urlobj.search) : "";
+    }
+
+    // params解析
+    function parseQueryString(search) {
+      let pairs = search.slice(1).split("&");
+      let paramsMap = pairs.map(pair => {
+        let [key, value] = pair.split("=");
+        return [decodeURIComponent(key), decodeURIComponent(value)];
+      }).reduce((res, [key, value]) => Object.assign(res, { [key]: value }), {});
+      return paramsMap || "";
+    }
+
+    return defaultParserResult;
+  } catch (err) {
+    throw new Error("输入的url格式错误:" + err)
+  }
 };
 
 // 测试用例
@@ -22,12 +46,14 @@ parserUrl('https://baidu.com:443/s?wd=hello');
 
 function App() {
   const [result, setResult] = useState(defaultParserResult);
-
+  const urlRef = useRef()
   useEffect(() => {
     const onKeyDown = (e) => {
       if (e.keyCode === 13) {
         console.log('这里 处理 Enter 事件');
-        setResult(defaultParserResult);
+        // setState 当前Ref <input>元素的value , 这里亦可使用e.target.value
+        setResult({ ...parserUrl(urlRef.current.value) });
+        // enter后清空输入框: urlRef.current.value = ""
       }
     }
     document.addEventListener('keydown', onKeyDown, false);
@@ -41,8 +67,8 @@ function App() {
         <div>并将结果渲染在页面上（tips: 请尽可能保证 parserUrl 的健壮性和完备性）</div>
       </header>
       <section className="App-content">
-        <input type="text" placeholder="请输入 url 字符串" />
-        <button id="J-parserBtn">解析</button>
+        <input ref={urlRef} type="text" placeholder="请输入 url 字符串" />
+        <button id="J-parserBtn" onClick={() => setResult({ ...parserUrl(urlRef.current.value) })}>解析</button>
       </section>
       <section className="App-result">
         <h2>解析结果</h2>
